2ff12b423932630b912903d9f283afdf3f347a29
rahulgupta177 <81315852+rahulgupta177@users.noreply.github.com>
2021-03-28 20:14:45 +0530

    Create fibonacci_series.v

---
+++ Day_3/Codes/fibonacci_series.v
@@ -0,0 +1,22 @@
+\m4_TLV_version 1d: tl-x.org
+\SV
+
+   // =========================================
+   // Welcome!  Try the tutorials via the menu.
+   // =========================================
+
+   // Default Makerchip TL-Verilog Code Template
+   
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m4_makerchip_module   // (Expanded in Nav-TLV pane.)
+\TLV
+   $reset = *reset;
+
+   $val[15:0] = $reset ? 1 : (>>1$val + >>2$val);
+
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
